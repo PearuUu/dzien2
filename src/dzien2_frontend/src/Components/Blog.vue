a1406c0c319b9b56d2ab6e9e3177b16fbbb07f9c
Pearu <bartekg100@gmail.com>
2024-06-25 17:52:50 +0200

    add dodajWpis()

--- src/dzien2_frontend/src/Components/Blog.vue
+++ src/dzien2_frontend/src/Components/Blog.vue
@@ -3,6 +3,9 @@
         <button @click="pobierzWpisy">Pobierz</button>
         Siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        {{ nowyBlog }}
+        <button @click="dodajWpis">Dodaj</button>
     </div>
 </template>
 
@@ -13,11 +16,17 @@ export default {
     data()
     {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
 
     methods: {
+        async dodajWpis()
+        {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
+
         async pobierzWpisy()
         {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
