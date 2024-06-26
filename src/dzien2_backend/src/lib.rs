e5fd732450ea00d54b819b72c05ca1431bfa772d
Pearu <bartekg100@gmail.com>
2024-06-26 15:38:02 +0200

    add usun_wpis(), add edytuj_wpis()

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell};
+use std::cell::{RefCell, RefMut};
 
 thread_local! {
     static WPISY: RefCell<Vec<String>> = RefCell::default();
@@ -19,4 +19,19 @@ fn dodaj_wpis(wpis: String) {
 #[ic_cdk::query]
 fn odczytaj_wpisy() -> Vec<String> {
     WPISY.with(|wpisy: &RefCell<Vec<String>>| wpisy.borrow().clone())
+}
+
+#[ic_cdk::update]
+fn usun_wpis(id_wpisu: usize) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| wpisy.borrow_mut().remove(id_wpisu));
+}
+
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        let mut binding: RefMut<Vec<String>> = wpisy.borrow_mut();
+        let mut wpis: Option<&mut String> = binding.get_mut(id_wpisu);
+        let stary: &mut String = wpis.unwrap();
+        *stary = nowy_wpis;
+    });
 }
