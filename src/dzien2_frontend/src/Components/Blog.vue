3a78d6a1c9b3bf0238b8e98c863c1885339847b2
Pearu <bartekg100@gmail.com>
2024-06-26 17:15:55 +0200

    dzien3

--- src/dzien2_frontend/src/Components/Blog.vue
+++ src/dzien2_frontend/src/Components/Blog.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">Pobierz</button>
-        Siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        {{ nowyBlog }}
-        <button @click="dodajWpis">Dodaj</button>
+        
+        <h1 class="text-blue-600">Wpisy na bloga:</h1>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="text-green-200 bg-blue-600 rounded-md p-4">Pobierz</button>
+        </div>
+        
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy"
+            class="drop-shadow-x1 bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+
+        <div class="flex justify-center">
+        <input v-model="nowyBlog" type="text"
+            class="border-2 border-blue-600 p-4 rounded-md w-100">
+            
+            <button @click="dodajWpis" class="text-green-200 bg-blue-600 rounded-md p-4 ml-4">Dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -25,12 +38,16 @@ export default {
         async dodajWpis()
         {
             await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            this.pobierzWpisy();
         },
 
         async pobierzWpisy()
         {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
+    },
+    async mounted() {
+        this.pobierzWpisy();
     }
 }
 
